dde5e691f574f141b1343f037e762afda32b240b
[dyad] <tchosco1995@gmail.com>
2025-12-28 15:38:06 -0300

    [dyad] Lendo arquivos alvo e buscando imports antigos do Supabase - wrote 3 file(s)

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,17 +1,14 @@
 import * as React from "react";
 import { GripVertical } from "lucide-react";
-// REPLACED: imports nomeados (quebrando na versão instalada)
-// import { PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels";
-import * as RSP from "react-resizable-panels";
+import { PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels";
 
 import { cn } from "@/lib/utils";
 
-// ADDED: resolução via namespace (compatível com a biblioteca instalada)
-const PanelGroup = (RSP as any).PanelGroup as React.ComponentType<any>;
-const Panel = (RSP as any).Panel as React.ComponentType<any>;
-const PanelResizeHandle = (RSP as any).PanelResizeHandle as React.ComponentType<any>;
+type ResizablePanelGroupProps = React.ComponentProps<typeof PanelGroup> & {
+  className?: string;
+};
 
-const ResizablePanelGroup = ({ className, ...props }: any) => (
+const ResizablePanelGroup = ({ className, ...props }: ResizablePanelGroupProps) => (
   <PanelGroup
     className={cn("flex h-full w-full data-[panel-group-direction=vertical]:flex-col", className)}
     {...props}
@@ -20,11 +17,16 @@ const ResizablePanelGroup = ({ className, ...props }: any) => (
 
 const ResizablePanel = Panel;
 
+type ResizableHandleProps = React.ComponentProps<typeof PanelResizeHandle> & {
+  withHandle?: boolean;
+  className?: string;
+};
+
 const ResizableHandle = ({
   withHandle,
   className,
   ...props
-}: { withHandle?: boolean; className?: string } & any) => (
+}: ResizableHandleProps) => (
   <PanelResizeHandle
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 [&[data-panel-group-direction=vertical]>div]:rotate-90",
